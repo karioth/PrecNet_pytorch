992b4e72d2c60cd51d369ab814cc555f6926bd35
karioth <149701938+karioth@users.noreply.github.com>
2024-04-28 11:03:40 +0200

    Update kitti_data.py

--- kitti_data.py
+++ kitti_data.py
@@ -2,13 +2,25 @@ import hickle as hkl
 import torch
 import torch.utils.data as data
 
+import h5py
+import torch
+import torch.utils.data as data
+import numpy as np
+
 class KITTI(data.Dataset):
-    def __init__(self, datafile, sourcefile, nt):
+    def __init__(self, datafile, sourcefile, nt = 10):
         self.datafile = datafile
         self.sourcefile = sourcefile
-        self.X = hkl.load(self.datafile)
-        self.sources = hkl.load(self.sourcefile)
+
+        with h5py.File(self.datafile, 'r') as f:
+            key = list(f.keys())[0]
+            self.X = f[key][:] #X will be like (n_images, nb_cols, nb_rows, nb_channels)
+        with h5py.File(self.sourcefile, 'r') as f:
+            key = list(f.keys())[0]
+            self.sources = f[key][:]
+
         self.nt = nt
+
         cur_loc = 0
         possible_starts = []
         while cur_loc < self.X.shape[0] - self.nt + 1:
@@ -21,7 +33,11 @@ class KITTI(data.Dataset):
 
     def __getitem__(self, index):
         loc = self.possible_starts[index]
-        return self.X[loc:loc+self.nt]
+        seq = self.preprocess(self.X[loc:loc+self.nt])
+        return seq
 
     def __len__(self):
         return len(self.possible_starts)
+
+    def preprocess(self, X):
+        return X.astype(np.float32) / 255.
